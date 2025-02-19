6a1fe5fc42518ab530e036ebc483e7dbbec4fd5d
JunEvergreen <83171358+JunEvergreen@users.noreply.github.com>
2025-02-19 15:23:01 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'FileManagerApp',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  fileName: string = '';
+  fileContent: string | any = '';
+  fileList: string[] = [];
+
+  constructor(private router: Router) {}
+
+  async createFile() {
+    try {
+      await Filesystem.writeFile({
+        path: this.fileName,
+        data: this.fileContent,
+        directory: Directory.Documents,
+      });
+      alert('File created successfully');
+      this.refreshFileList();
+    } 
+    catch (error) {
+      console.error('Error creating file', error);
+    }
+  }
+
+  async readFile() {
+    try {
+      const contents = await Filesystem.readFile({
+        path: this.fileName,
+        directory: Directory.Documents,
+      });
+      this.fileContent = contents.data;
+      alert('File Content loaded!');
+    } catch (error) {
+      console.error('Error reading file', error);
+    }
+  }
+
+  async deleteFile() {
+    try {
+      await Filesystem.deleteFile({
+        path: this.fileName,
+        directory: Directory.Documents,
+      });
+      alert('File deleted successfully!');
+      this.refreshFileList();
+    }
+    catch(error) {
+      console.error('Error deleting file', error);
+    }
+  }
+
+  async refreshFileList() {
+    try {
+      const result = await Filesystem.readdir({
+        path: '',
+        directory: Directory.Documents,
+      });
+      this.fileList = result.files.map(file => file.name);
+    }
+    catch(error) {
+      console.error('Error reading directory', error);
+    }
+  }
+
+  goToFiles() {
+    this.router.navigate(['/files']);
+  }
+}
